af4a10a59fb4ac3e2967244fee169d49a3ea10fe
Leonel <leonelgiralde22@gmail.com>
2025-02-24 18:29:00 -0300

    rutas vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import { BrowserRouter as Router } from "react-router-dom";
-import AppRoutes from "./routes/AppRoutes";
+import AppRoutes from "./routes/AppRoutes"; // Asegúrate de que la ruta es correcta
 
 function App() {
-  
   return (
-    <>
-      <Router>
-        <AppRoutes />
-     </Router>
-    </>
-  )
+    <Router>
+      <AppRoutes />
+    </Router>
+  );
 }
 
-export default App
+export default App;
