d412e39cfc579a87e1615977b0431059d5665f4d
Leonel <leonelgiralde22@gmail.com>
2025-02-27 13:09:27 -0300

    sacando misreportes

--- src/views/InicioAdmin/InicioAdmin.jsx
+++ src/views/InicioAdmin/InicioAdmin.jsx
@@ -1,12 +1,11 @@
 import Home from "../../components/Home/Home";
-import MisReportes from "../../components/MisReportes/MisReportes";
 
 function ViewHome() {
     return (
         <div>
         
         <Home/>
-        <MisReportes/>
+        
         
         </div>
     );
