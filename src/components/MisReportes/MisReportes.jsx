da9748fec5d31a6e34c776769d8a2f602406ebad
Leonel <leonelgiralde22@gmail.com>
2025-03-01 19:17:18 -0300

    cambio de ruta

--- src/components/MisReportes/MisReportes.jsx
+++ src/components/MisReportes/MisReportes.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { conexionAPI } from "../../conexionAPI";
+import { conexionAPI } from "../../ConexionAPI";
 
 const MisReportes = () => {
   const [reportes, setReportes] = useState([]);
