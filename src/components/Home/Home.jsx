13d076ea6e76573aece82b94a0b08b2f90f06f32
Leonel <leonelgiralde22@gmail.com>
2025-03-01 20:47:04 -0300

    arreglando error

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,13 @@
 import ButtonNov from '../ButtonNov/ButtonNov';
 import ButtonForm from '../ButtonRep/ButtonRep';
-
+import MisReportes from '../MisReportes/MisReportes';
 function Home() {
     return (
         <div>
         <h2>Hola! ¿Que vamos a subir hoy? </h2>
         <ButtonNov/>
         <ButtonForm/>
+        <MisReportes/>
         </div>
     );
 }
--- src/views/InicioAdmin/InicioAdmin.jsx
+++ src/views/InicioAdmin/InicioAdmin.jsx
@@ -1,12 +1,12 @@
 import Home from "../../components/Home/Home";
-import Reports from "../../Components/MisReportes/MisReportes";
+
 function ViewHome() {
     return (
         <div>
         
         <Home/>
     
-        <Reports/>
+        
 
         </div>
     );
