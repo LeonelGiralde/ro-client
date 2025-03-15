720e6637776e641c82a01386675931284c5972fd
Leonel <leonelgiralde22@gmail.com>
2025-03-15 10:12:19 -0300

    cambios misreportes3

--- src/components/MisReportes/MisReportes.jsx
+++ src/components/MisReportes/MisReportes.jsx
@@ -1,25 +1,16 @@
-import { useEffect, useState } from "react";
+/*import { useEffect, useState } from "react";
 const API_URL = import.meta.env.VITE_API_URL;
+*/
 
 function Reportes() {
   console.log("Componente Reportes montado");
-  const [reportes, setReportes] = useState([]);
-
-  useEffect(() => {
-    fetch(`${API_URL}`)
-      .then((response) => response.json())
-      .then((data) => setReportes(data))
-      .catch((error) => console.error("Error al obtener reportes:", error));
-}, []);
+ 
+ 
 
   return (
     <div>
       <h2>Lista de Reportes</h2>
-      <ul>
-        {reportes.map((reporte) => (
-          <li key={reporte._id}>{reporte.nombre} - {reporte.descripcion}</li>
-        ))}
-      </ul>
+      
     </div>
   );
 }
